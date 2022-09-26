53310b81dc8e9f154db6578ab4ce12deb2a6c4dc
Murilo Henrique Prudencio <murilo.prudencio@outlook.com>
2022-09-25 22:39:06 -0300

    Ajustando as telas

--- js/main.js
+++ js/main.js
@@ -13,3 +13,26 @@ function mostrarTempo() {
 mostrarTempo();
 setInterval(mostrarTempo, 1000);
 })
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  autoplay: true,
+  autoplayTimeout: 2500,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:5,
+          nav:true,
+          loop:false
+      }
+  }
+})
